10ad2737a084beb10db1070368ee30d7fb6152c8
sengokyu <sengokyu+gh@gmail.com>
2021-01-02 19:44:38 +0900

    Implements some

--- bugyocloudproxy/app.py
+++ bugyocloudproxy/app.py
@@ -1,10 +1,47 @@
 import json
+from logging import Logger, getLogger
+from typing import Any
 
-# import requests
+from bugyocloudclient import AuthInfo, BugyoCloudClient
+from bugyocloudclient.models.punchinfo import ClockType, PunchInfo
+from bugyocloudclient.tasks.logintask import LoginTask
+from bugyocloudclient.tasks.punchtask import PunchTask
+
+logger = getLogger(__name__)
+
+
+class EventBody:
+    def __init__(self, event: dict) -> None:
+        if 'body' not in event:
+            logger.critical('"body" is not presented.')
+
+        self._body = json.loads(event['body'])
+
+    def __getattr__(self, name: str) -> Any:
+        if name not in self._body:
+            logger.critical('"{}" is not presented'.format(name))
+        return self._body[name]
+
+
+def create_client(tenant_code: str) -> BugyoCloudClient:
+    return BugyoCloudClient(tenant_code)
+
+
+def create_logintask(login_id: str, password: str) -> LoginTask:
+    auth_info = AuthInfo(login_id, password)
+    return LoginTask(auth_info)
+
+
+def create_punchtask(clock_type: str) -> PunchTask:
+    punch_info = PunchInfo()
+    punch_info.clock_type = ClockType[clock_type]
+    punch_info.latitude = 35.6812
+    punch_info.longitude = 139.7742
+    return PunchTask(punch_info)
 
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
+    """Bugyo Cloud proxy
 
     Parameters
     ----------
@@ -25,18 +62,17 @@ def lambda_handler(event, context):
         Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
     """
 
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
+    body = EventBody(event)
+    client = create_client(body.tenant_code)
+    login_task = create_logintask(body.login_id, body.password)
+    punch_task = create_punchtask(body.clock_type)
 
-    #     raise e
+    client.exec(login_task)
+    client.exec(punch_task)
 
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
+            "result": "OK",
         }),
     }
--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -1,64 +1,47 @@
 import json
 
 import pytest
-
-from hello_world import app
+from bugyocloudproxy import app
 
 
 @pytest.fixture()
 def apigw_event():
     """ Generates API GW Event"""
-
     return {
-        "body": '{ "test": "body"}',
-        "resource": "/{proxy+}",
+        "version": "2.0",
+        "routeKey": "$default",
+        "rawPath": "/my/path",
+        "rawQueryString": "",
+        "cookies": [],
+        "headers": {},
+        "queryStringParameters": {},
         "requestContext": {
-            "resourceId": "123456",
-            "apiId": "1234567890",
-            "resourcePath": "/{proxy+}",
-            "httpMethod": "POST",
-            "requestId": "c6af9ac6-7b61-11e6-9a41-93e8deadbeef",
             "accountId": "123456789012",
-            "identity": {
-                "apiKey": "",
-                "userArn": "",
-                "cognitoAuthenticationType": "",
-                "caller": "",
-                "userAgent": "Custom User Agent String",
-                "user": "",
-                "cognitoIdentityPoolId": "",
-                "cognitoIdentityId": "",
-                "cognitoAuthenticationProvider": "",
-                "sourceIp": "127.0.0.1",
-                "accountId": "",
+            "apiId": "api-id",
+            "domainName": "id.execute-api.us-east-1.amazonaws.com",
+            "domainPrefix": "id",
+            "http": {
+                "method": "POST",
+                "path": "/my/path",
+                "protocol": "HTTP/1.1",
+                "sourceIp": "IP",
+                "userAgent": "agent"
             },
-            "stage": "prod",
+            "requestId": "id",
+            "routeKey": "$default",
+            "stage": "$default",
+            "time": "12/Mar/2020:19:03:58 +0000",
+            "timeEpoch": 1583348638390
         },
-        "queryStringParameters": {"foo": "bar"},
-        "headers": {
-            "Via": "1.1 08f323deadbeefa7af34d5feb414ce27.cloudfront.net (CloudFront)",
-            "Accept-Language": "en-US,en;q=0.8",
-            "CloudFront-Is-Desktop-Viewer": "true",
-            "CloudFront-Is-SmartTV-Viewer": "false",
-            "CloudFront-Is-Mobile-Viewer": "false",
-            "X-Forwarded-For": "127.0.0.1, 127.0.0.2",
-            "CloudFront-Viewer-Country": "US",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-            "Upgrade-Insecure-Requests": "1",
-            "X-Forwarded-Port": "443",
-            "Host": "1234567890.execute-api.us-east-1.amazonaws.com",
-            "X-Forwarded-Proto": "https",
-            "X-Amz-Cf-Id": "aaaaaaaaaae3VYQb9jd-nvCd-de396Uhbp027Y2JvkCPNLmGJHqlaA==",
-            "CloudFront-Is-Tablet-Viewer": "false",
-            "Cache-Control": "max-age=0",
-            "User-Agent": "Custom User Agent String",
-            "CloudFront-Forwarded-Proto": "https",
-            "Accept-Encoding": "gzip, deflate, sdch",
-        },
-        "pathParameters": {"proxy": "/examplepath"},
-        "httpMethod": "POST",
-        "stageVariables": {"baz": "qux"},
-        "path": "/examplepath",
+        "body": json.dumps({
+            "tenant_code": "12345",
+            "login_id": "oneuser",
+            "password": "pass",
+            "clock_type": "clock_in"
+        }),
+        "pathParameters": {},
+        "isBase64Encoded": False,
+        "stageVariables": {}
     }
 
 
